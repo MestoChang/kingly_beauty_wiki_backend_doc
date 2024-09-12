51b9791ace1d1b8b82aca553ba9688e391dc5660
Yenchen <k307849@gmail.com>
2024-09-12 10:06:01 +0800

    Updates

--- assets/js/acecf23e.631f3f91.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkingly_beauty_wiki_backend_doc=self.webpackChunkkingly_beauty_wiki_backend_doc||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/kingly_beauty_wiki_backend_doc/blog","blogTitle":"Blog","authorsListPath":"/kingly_beauty_wiki_backend_doc/blog/authors"}')}}]);
